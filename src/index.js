160a0f7d19ad1dbe9f76b23ba5b9af5e528c05f6
mgoodrich <mark.goodrich@live.com>
2018-11-01 14:00:02 -0400

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'babel-polyfill';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
