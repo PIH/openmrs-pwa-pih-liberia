0db3edbb60203277c949b6926c3d68dfe20efc5b
mgoodrich <mark.goodrich@live.com>
2018-11-01 14:53:17 -0400

    set up store.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Switch } from 'react-router-dom';
+import { LoadingView } from '@openmrs/react-components';
 import './App.css';
+import setupStoreAndPersistor, { history } from './store';
+import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const { store, persistor } = setupStoreAndPersistor();
+
+const App = props => {
+
+  return (
+    <Provider store={store}>
+
+      <PersistGate loading={<LoadingView />} persistor={persistor}>
+        <ConnectedRouter history={history}>
+          <Switch>
+
+          </Switch>
+        </ConnectedRouter>
+      </PersistGate>
+
+    </Provider>
+  );
+
+};
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,88 @@
+/* * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+import { createHashHistory } from 'history';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
+import { reducer as reduxFormReducer } from 'redux-form';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { sagas as openmrsSagas, reducers as openmrsReducers, LOGIN_TYPES } from '@openmrs/react-components';
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+// fyi, connected-react-router docs:
+// https://github.com/supasate/connected-react-router
+
+const contextPath  = (typeof process !== 'undefined' && typeof process.env !== 'undefined' &&
+  typeof process.env.REACT_APP_CONTEXT_PATH  !== 'undefined' && process.env.REACT_APP_CONTEXT_PATH !== null) ?
+  process.env.REACT_APP_CONTEXT_PATH : "/";
+
+export const history = createHashHistory({
+  basename: contextPath
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [
+  routerMiddleware(history),
+  sagaMiddleware
+];
+
+/**
+ * Elements in the store:
+ *
+ * openmrs: wires in the reducers provided by the openmrs-reactcomponents module
+ */
+
+
+const combinedReducer = combineReducers({
+  openmrs: openmrsReducers,
+  form: reduxFormReducer
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGIN_TYPES.LOGOUT.SUCCEEDED) {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
+
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+  stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+const rootSagas = function* () {
+  yield all([
+    openmrsSagas(),
+  ]);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
+
+export default () => {
+  const store = createStore(
+    connectRouter(history)(pReducer),
+    compose(
+      applyMiddleware(...middlewares),
+      window.devToolsExtension && process.env.NODE_ENV !== 'production'
+        ? window.devToolsExtension() : f => f,
+    ));
+  sagaMiddleware.run(rootSagas);
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
+
