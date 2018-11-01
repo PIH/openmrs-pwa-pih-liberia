8458be12d289f996f9ae1d929faba9fc37e073a8
mgoodrich <mark.goodrich@live.com>
2018-11-01 14:02:00 -0400

    add css files for ag-grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'ag-grid/dist/styles/ag-grid.css';
+import 'ag-grid/dist/styles/ag-theme-material.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
