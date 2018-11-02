76bfefee264406863c8c538cc9ab2441ec5f1a97
mgoodrich <mark.goodrich@live.com>
2018-11-02 11:12:56 -0400

    add font awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
+import {
+  faBars,
+  faEdit,
+  faUser,
+  faPills,
+  faMapMarker,
+  faCog,
+  faKey,
+  faSignOutAlt,
+  faRibbon,
+  faVial,
+  faChild,
+  faSearch,
+  faNotesMedical,
+  faHeart,
+  faUserMd,
+  faLemon,
+  faHome
+} from '@fortawesome/free-solid-svg-icons';
 import { Logout, LoadingView } from '@openmrs/react-components';
 import './App.css';
 import setupStoreAndPersistor, { history } from './store';
@@ -9,6 +29,9 @@ import { PersistGate } from 'redux-persist/lib/integration/react';
 import LoginPage from './login/LoginPage'
 import Layout from './layout/Layout'
 import HomePage from './home/HomePage'
+
+// add any more fonts you wish to use to the import above and to here
+fontAwesomeLibrary.add(faBars, faEdit, faPills, faUser, faMapMarker, faCog, faKey, faSignOutAlt, faRibbon, faVial, faChild, faSearch, faNotesMedical, faHeart, faUserMd, faLemon, faHome);
 
 const { store, persistor } = setupStoreAndPersistor();
 
