3318345e2649cc69a7288b2d2ad958ccea3e6bc4
mgoodrich <mark.goodrich@live.com>
2018-11-05 14:20:37 -0500

    add babel-polyfill register service worker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'ag-grid/dist/styles/ag-grid.css';
@@ -14,4 +15,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
